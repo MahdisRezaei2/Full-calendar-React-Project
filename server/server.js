edd769ce9a669da7bbe17d15efffe95f8d1e6715
MahdisRezaei2 <mahdis.re20@gmail.com>
2023-09-25 17:31:18 -0700

    first commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGODB_URI, {
+
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+}, () => console.log("connected to Mongodb"));
+
+app.use("/api/calendar", require("./Controllers/CalendarController"));
+
+app.listen(3001, () => console.log("server started"));
